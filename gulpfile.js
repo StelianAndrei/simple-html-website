84420686c663e72e779d6159a197a9f8d25e526d
Stelian Andrei <stelian.andrei@gmail.com>
2015-03-09 14:59:59 +0200

    HTML and image minification + small fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,16 @@ var gulp = require('gulp'),
     // less/css
     autoprefixer = require('gulp-autoprefixer'),
     less = require('gulp-less'),
-    minify = require('gulp-minify-css'),
+    minifyCSS = require('gulp-minify-css'),
     // javascript
     uglify = require('gulp-uglify'),
     jshint = require('gulp-jshint'),
     // html
     include = require('gulp-file-include'),
+    minifyHTML = require('gulp-minify-html'),
+    // images
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
     // general
     concat = require('gulp-concat'),
     del = require('del'),
@@ -51,14 +55,23 @@ gulp.task('html', ['clean-html'], function(){
         .pipe(include({ 
             basepath: 'src/'
         }))
+        .pipe(minifyHTML({
+            empty: true,
+            conditionals: true
+        }))
         .pipe(gulp.dest('./build/'))
         .pipe(connect.reload());
     gulp.src('src/.htaccess').pipe(gulp.dest('./build/'));
 });
 
 // images
-gulp.task('images', ['clea-img'], function(){
-    gulp.src('src/images/**/*.*')
+gulp.task('images', ['clean-img'], function(){
+    gulp.src('src/images/**/*.*').
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
         .pipe(gulp.dest('./build/images/'))
         .pipe(connect.reload());
 });
@@ -71,7 +84,7 @@ gulp.task('less', ['clean-css'], function(){
             ])
         .pipe(less())
         .pipe(autoprefixer())
-        .pipe(minify())
+        .pipe(minifyCSS())
         .pipe(gulp.dest('./build/css/'))
         .pipe(connect.reload());
     // move bootstrap files
@@ -96,3 +109,4 @@ gulp.task('watch', function () {
 gulp.task('serve', ['connect', 'watch']);
 gulp.task('build', ['less', 'js', 'html', 'images']);
 gulp.task('default', ['serve']);
+
